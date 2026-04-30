1360ff9828ccfd2b948e66645c0f49f927dfedab
Heinrich <samuelsy2005@gmail.com>
2026-04-30 16:07:50 -0300

    first commit

---
+++ WindowWithButton.java
@@ -0,0 +1,15 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class WindowWithButton {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Window with Button");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+
+        JButton button = new JButton("Click Me");
+        frame.add(button);
+
+        frame.setVisible(true);
+    }
+}
