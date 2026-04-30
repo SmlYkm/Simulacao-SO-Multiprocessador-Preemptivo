48afd41dae006e9e2ee214adc5300346ee31ba2f
Heinrich <samuelsy2005@gmail.com>
2026-04-30 16:11:29 -0300

    asdfasdfadf

--- WindowWithButton.java
+++ WindowWithButton.java
@@ -1,6 +1,6 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
-
+//askjdhfaçlksdfh
 public class WindowWithButton {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Window with Button");
